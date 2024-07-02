69a58cac8c14a010500f1f36b3b83eb4fd435c3a
SDreadnoug2 <miltnr01@gmail.com>
2024-07-02 09:33:59 -0400

    upload

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,12 @@ const errors = require("../utils/errors");
 const auth = require("../middleware/auth");
 const { login, createUser } = require("../controllers/user");
 
+
 router.post("/signin", login);
 router.post("/signup", createUser);
-
 router.use("/users", auth, userRouter);
-
 router.use("/items", auth, itemRouter);
-
 router.use((req, res) => {
   res.status(errors.NOT_FOUND).send({ message: "Route not found" });
 });
-
 module.exports = router;
