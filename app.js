1d96abcada76127c9e9c7bf78e34457c41c06026
SDreadnoug2 <miltnr01@gmail.com>
2024-07-02 11:12:18 -0400

    Sprint13

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const helmet = require('helmet')
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://localhost:27017/wtwr_db");
 app.use(cors());
+app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
