1f9cb45660ddd54cb95d9e7fdff291441fc78db9
SDreadnoug2 <miltnr01@gmail.com>
2024-09-01 12:43:10 -0400

    fixes

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,16 +1,16 @@
-const {Joi, celebrate} = require('celebrate');
+const { Joi, celebrate } = require('celebrate');
 const validator = require('validator');
+
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
     return value;
   }
   return helpers.error('string.uri');
-}
+};
 
-
-export const clothingItemValidation = celebrate({
+const clothingItemValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30).messagages({
+    name: Joi.string().required().min(2).max(30).messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
@@ -20,9 +20,9 @@ export const clothingItemValidation = celebrate({
       'string.uri': 'The "imageUrl" field must be a valid url.',
     })
   })
-})
+});
 
-export const createUserValidation = celebrate({
+const createUserValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     avatar: Joi.string().required().uri(),
@@ -31,17 +31,22 @@ export const createUserValidation = celebrate({
   }),
 });
 
-export const loginValidation = celebrate({
+const loginValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
 
-export const validateId = celebrate({
+const validateId = celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().length(24).required(),
   }),
 });
 
-
+module.exports = {
+  clothingItemValidation,
+  createUserValidation,
+  loginValidation,
+  validateId,
+};
