f3c15a00db8d2f2f8cd1d9d8d40160f7345d5663
SDreadnoug2 <miltnr01@gmail.com>
2024-09-11 09:29:35 -0400

    smallfix

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const itemRouter = require("./clothingItems");
 const auth = require("../middleware/auth");
 const { login, createUser } = require("../controllers/user");
 const {createUserValidation, loginValidation} = require('../middleware/validation');
-
+const NotFoundError = require("../utils/NotFoundError");
 const router = express.Router();
 router.post("/signin", loginValidation, login);
 router.post("/signup", createUserValidation, createUser);
 router.use("/users", auth, userRouter);
 router.use("/items", itemRouter);
+router.use((req, res, next) => {
+  next(new NotFoundError("Route not found."));
+});
 module.exports = router;
